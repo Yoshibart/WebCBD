9ed5536f2ee312b99807af9704c059287a290e3c
Barnet Obeka <usyb6kmawfhpt9a6grfh1ha3rlmqax@bots.bitbucket.org>
2026-03-15 22:50:40 +0000

    Solved the SonarQube Issues

--- src/test/java/com/mase/security/ApplicationUserDetailsTest.java
+++ src/test/java/com/mase/security/ApplicationUserDetailsTest.java
@@ -1,7 +1,6 @@
 package com.mase.security;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collection;
 
